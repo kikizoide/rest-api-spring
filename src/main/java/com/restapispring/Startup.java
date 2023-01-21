b2a32c36e71774057617307d7a6487ac96394c55
luish <luis@didows.com.br>
2023-01-21 10:27:09 -0300

    Refactor Startup e StartupTests

--- src/main/java/com/restapispring/Startup.java
+++ src/main/java/com/restapispring/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApiSpringApplication {
+public class Startup {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestApiSpringApplication.class, args);
+        SpringApplication.run(Startup.class, args);
     }
 
 }
--- src/test/java/com/restapispring/StartupTests.java
+++ src/test/java/com/restapispring/StartupTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RestApiSpringApplicationTests {
+class StartupTests {
 
     @Test
     void contextLoads() {
